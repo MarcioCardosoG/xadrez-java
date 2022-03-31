61ce5676b47714c2ec94449fd8b97732ee5ffe5c
Márcio Cardoso Gonçalves <96893959+MarcioCardosoG@users.noreply.github.com>
2022-03-30 21:49:33 -0300

    Add files via upload

--- src/boardgame/Tabuleiro.java
+++ src/boardgame/Tabuleiro.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+
+
 public class Tabuleiro {
 	
 	private int linhas;
@@ -8,6 +10,9 @@ public class Tabuleiro {
 	
 	
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas <1) {
+			throw new TabuleiroException("Erro de criação: é necessário pelomenos 1 linha e 1 coluna");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -19,27 +24,49 @@ public class Tabuleiro {
 	}
 
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 
 	public int getColunas() {
 		return colunas;
 	}
 
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-	
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExiste(linha, coluna)) {
+			throw new TabuleiroException("Posição não existe");
+		}
 		return pecas[linha][coluna];
 	}
 	
 	public Peca peca (Position position) {
+		if (!posicaoExiste(position)) {
+			throw new TabuleiroException("Posição não existe");
+		}
+		
 		return pecas[position.getLinha()][position.getColuna()];
 	}
 	
+	public void colocaPeca (Peca peca, Position position) {
+		if(issoEUmaPeca(position)) {
+			throw new TabuleiroException("Já existe uma peça nessa posição " + position);
+		}
+		pecas[position.getLinha()][position.getColuna()] = peca;
+		peca.position = position;
+	}
+	
+	private boolean posicaoExiste(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >=0 && coluna < colunas;
+		
+	}
+	
+	public boolean posicaoExiste(Position position) {
+		return posicaoExiste(position.getLinha(), position.getColuna());
+	}
+	
+	public boolean issoEUmaPeca(Position position) {
+		if (!posicaoExiste(position)) {
+			throw new TabuleiroException("Posição não existe");
+		}
+		return peca(position) !=null;
+	}
 
 }
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,24 +1,33 @@
 package xadrez;
 
+import boardgame.Position;
 import boardgame.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaXadrez {
-	
+
 	private Tabuleiro tabuleiro;
-	
+
 	public PartidaXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		iniciarPartida();
 	}
-	
-	public XadrezPeca[][] pecas(){
+
+	public XadrezPeca[][] pecas() {
 		XadrezPeca[][] partPecas = new XadrezPeca[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for (int i=0; i<tabuleiro.getLinhas();i++) {
-			for (int j=0; j<tabuleiro.getColunas();j++) {
-				partPecas[i][j] = (XadrezPeca) tabuleiro.peca(i,j);
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				partPecas[i][j] = (XadrezPeca) tabuleiro.peca(i, j);
 			}
 		}
 		return partPecas;
 	}
-	
+
+	private void iniciarPartida() {
+		tabuleiro.colocaPeca(new Torre(tabuleiro, Cor.BRANCA), new Position(2, 1));
+		tabuleiro.colocaPeca(new Rei(tabuleiro, Cor.PRETA), new Position(0, 4));
+		tabuleiro.colocaPeca(new Rei(tabuleiro, Cor.BRANCA), new Position(7, 4));
+	}
 
 }
